a411ae81ee20457bdef4b96a939bd805ce4f9dfb
lixueqiang <lixueqiang@croot.com>
2023-11-22 18:09:05 +0800

    init

---
+++ src/test/java/com/xqlim/demo/spr/SprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xqlim.demo.spr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
